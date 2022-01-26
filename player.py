e24b21f8aeff595ab971440cdba24378cc8a8dd8
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-01-26 10:49:42 +0100

    bet 1k for high pair

--- player.py
+++ player.py
@@ -6,11 +6,16 @@ class Player:
             call_amount = game_state['current_buy_in'] - game_state['players'][game_state['in_action']]['bet']
             my_player = game_state['players'][game_state['in_action']]
             my_cards = my_player['hole_cards']
+            community_cards = game_state['community_cards']
             current_round = game_state['round']
 
             if (self.is_pair(my_cards[0], my_cards[1])):
-                print('bet (is pair): 200')
-                return 200
+                if self.is_high_card(my_cards[0]):
+                    print('bet (is high pair): 1000')
+                    return 1000
+                else:
+                    print('bet (is pair): 200')
+                    return 200
 
             if current_round == 0:
                 print('bet: ', game_state['small_blind'] * 2)
@@ -30,10 +35,15 @@ class Player:
     def showdown(self, game_state):
         pass
 
-    
+    def has_triplet(cards):
+        return
 
     def is_pair(self, card1, card2):
         return card1['rank'] == card2['rank']
+
+    def is_high_card(self, card):
+        return card['rank'] in ['A', 'K', 'Q', 'J', '10']
+
 
     
 
