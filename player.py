2a69c6901c7e6f3ee22c8c1efd624eff71cdc034
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-01-26 10:02:09 +0100

    always bet 1k

--- player.py
+++ player.py
@@ -5,12 +5,13 @@ class Player:
         return card1['rank'] == card2['rank']
 
     def betRequest(self, game_state):
-        my_player = game_state['players']['in_action']
-        my_cards = my_player['hole_cards']
-        if (is_pair(my_cards[0], my_cards[1])):
-            return 1000
+        return 1000
+        # my_player = game_state['players']['in_action']
+        # my_cards = my_player['hole_cards']
+        # if (is_pair(my_cards[0], my_cards[1])):
+        #     return 1000
 
-        return 0
+        # return 0
 
     def showdown(self, game_state):
         pass
