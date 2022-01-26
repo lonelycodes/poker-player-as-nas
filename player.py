d1b3f62aefc0b59c37777ee397f48036048b6ec6
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-01-26 10:11:29 +0100

    check pair and bet 10

--- player.py
+++ player.py
@@ -1,20 +1,19 @@
 class Player:
     VERSION = "Default Python folding player"
 
-    def is_pair(card1, card2):
-        return card1['rank'] == card2['rank']
-
     def betRequest(self, game_state):
-        return 1000
-        # my_player = game_state['players']['in_action']
-        # my_cards = my_player['hole_cards']
-        # if (is_pair(my_cards[0], my_cards[1])):
-        #     return 1000
+        my_player = game_state['players'][game_state['in_action']]
+        my_cards = my_player['hole_cards']
+        if (self.is_pair(my_cards[0], my_cards[1])):
+            return 1000
 
-        # return 0
+        return 10
 
     def showdown(self, game_state):
         pass
+
+    def is_pair(self, card1, card2):
+        return card1['rank'] == card2['rank']
 
     
 
---
+++ player_tests.py
@@ -0,0 +1,50 @@
+import unittest
+from player import Player
+
+class PlayerTests(unittest.TestCase):
+    def test_is_pair_bets_1k(self):
+        player = Player()
+        game_state = {
+            'in_action': 0,
+            'players': [
+                { 'hole_cards': [
+                        {
+                            'rank': '2',
+                            'suit': 'hearts'
+                        },
+                        {
+                            'rank': '2',
+                            'suit': 'spades'
+                        }
+                    ] }
+            ]
+                
+            }
+        
+        self.assertEqual(player.betRequest(game_state), 1000)
+
+    def test_no_pair_checks(self):
+        player = Player()
+        game_state = {
+            'in_action': 0,
+            'players': [
+                { 
+                    'hole_cards': [
+                        {
+                            'rank': '2',
+                            'suit': 'hearts'
+                        },
+                        {
+                            'rank': '3',
+                            'suit': 'spades'
+                        }
+                    ]
+                }
+            ]
+                
+            }
+        
+        self.assertEqual(player.betRequest(game_state), 0)
+
+if __name__ == '__main__':
+    unittest.main()
