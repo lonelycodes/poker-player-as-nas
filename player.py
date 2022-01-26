389730f2c1634a99e2332376d51279723588ae2f
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-01-26 15:06:40 +0100

    safety net

--- player.py
+++ player.py
@@ -16,8 +16,7 @@ class Player:
         print('community_cards:', community_cards)
         print('call_amount:', call_amount)
         print('minimum_raise', game_state['minimum_raise']) 
-
-        
+    
 
         if (self.has_n_tuple_with_my_card(my_cards, all_cards, 4) != None):
             return my_player['stack']
@@ -32,10 +31,13 @@ class Player:
             if self.is_high_card(my_cards[0]):
                 return my_player['stack']
             else:
-                if(call_amount < 500): 
+                if(call_amount < 500) and my_player['bet'] < 0.1 * my_player['stack']: 
                     return call_amount + game_state['minimum_raise'] + 1
                 else:
                     return 0
+
+        if len(community_cards) == 0 and my_player['bet'] > 0.1 * my_player['stack']:
+            return 0
 
         if self.is_high_card(my_cards[0]) and self.is_high_card(my_cards[1]):
             if(call_amount < 300): 
