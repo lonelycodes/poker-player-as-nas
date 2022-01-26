68cf7c4da7ea93aacb433571eca021e0eb69e65d
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-01-26 10:25:10 +0100

    look at call amount

--- player.py
+++ player.py
@@ -2,15 +2,29 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        my_player = game_state['players'][game_state['in_action']]
-        my_cards = my_player['hole_cards']
-        if (self.is_pair(my_cards[0], my_cards[1])):
-            return 1000
+        try:
+            call_amount = game_state['current_buy_in'] - game_state['players'][game_state['in_action']]['bet']
+            my_player = game_state['players'][game_state['in_action']]
+            my_cards = my_player['hole_cards']
+            current_round = game_state['round']
 
-        return game_state['small_blind']
+            if current_round == 0:
+                return game_state['small_blind'] * 2
+
+            if(call_amount > 100):
+                return 0
+
+            if (self.is_pair(my_cards[0], my_cards[1])):
+                return 1000
+
+            return game_state['small_blind']
+        except:
+            return 1
 
     def showdown(self, game_state):
         pass
+
+    
 
     def is_pair(self, card1, card2):
         return card1['rank'] == card2['rank']
