7f1d80557bba7fb76e8040b9b44f3de072c54947
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2022-01-26 10:45:33 +0100

    bet 200 for pairs always

--- player.py
+++ player.py
@@ -8,15 +8,15 @@ class Player:
             my_cards = my_player['hole_cards']
             current_round = game_state['round']
 
+            if (self.is_pair(my_cards[0], my_cards[1])):
+                print('bet (is pair): 200')
+                return 200
+
             if current_round == 0:
                 print('bet: ', game_state['small_blind'] * 2)
                 return game_state['small_blind'] * 2
 
             print('call amount: ', call_amount)
-
-            if (self.is_pair(my_cards[0], my_cards[1])):
-                print('bet (is pair): 200')
-                return 200
 
             if(call_amount > 100):    
                 print('bet (call amount larger than 100): 0')
